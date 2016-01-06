efbd5008f4e02964568415332981432a8077e52e
Andy <andy.addington@ganchrow.com>
2016-01-06 07:53:08 -0600

    bind each or bind all

--- lib/watch_redis_hash.rb
+++ lib/watch_redis_hash.rb
@@ -3,13 +3,13 @@ require 'json'
 # persists any object with redis hash command
 
 class WatchRedisHash
-  def initialize(client, key, subkey)
+  def initialize(client: nil, key: nil, subkey: nil, each: nil)
+    fail 'No Client provided to WatchRedisHash' unless @client
     @client = client
     @key = key
     @subkey = subkey
-    @obj = WatchHash.new.setbind(true) do |same_obj|
-      @client.hset(@key, @subkey, same_obj.to_json)
-    end
+    _bind_each if each
+    _bind unless each
   end
 
   def []=(key, val)
@@ -17,9 +17,37 @@ class WatchRedisHash
   end
 
   def load
-    @client.hget(@key, @subkey) do |res|
-      @obj.merge!(JSON.parse(res))
+    if @each
+      @client.hgetall(@key) do |res|
+        @obj.merge!(Hash[_parse_map_zip(res.keys, res.values)])
+      end
+    else
+      @client.hget(@key, @subkey) do |res|
+        @obj.merge!(JSON.parse(res))
+      end
     end
+  end
+
+  private
+
+  def _bind_each
+    @each = true
+    @obj = WatchHash.new.setbind do |(subkey, val)|
+      @client.hset(@key, subkey, val.to_json)
+    end
+  end
+
+  def _bind
+    @obj = WatchHash.new.setbind(true) do |same_obj|
+      @client.hset(@key, @subkey, same_obj.to_json)
+    end
+  end
+
+  def _parse_map_zip(keys, vals)
+    keys.map(&JSON.method(:parse))
+    .zip(
+      vals.map(&JSON.method(:parse))
+    )
   end
 end
 
